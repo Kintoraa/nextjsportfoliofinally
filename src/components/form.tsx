3694de7a0a95e75d27f5be91c7d5cf9938200389
alexiszanini <dev.kintora@gmail.com>
2025-04-07 16:37:00 +0200

    v.1.4

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -30,8 +30,8 @@ export default function Form() {
 
                 emailjs
                     .send(
-                        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-                        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+                        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+                        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
                         dataForm,
                         process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
                     )
